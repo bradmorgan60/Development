929dfa961e8e02390fe418bbc7e8a7d45688aaa5
Brad Morgan <jbradmorg16@gmail.com>
2024-05-04 10:47:07 -0400

    first commit

---
+++ API/Project1/index.js
@@ -0,0 +1,14 @@
+/* Javascript runtime
+ runtime is a program / environment that runs other programs; built on C++
+ runtime -> 
+ used for server-side, network apps, APIs
+*/
+
+const http = require("http");
+const fs = require("fs");
+const Person = require("./person");
+
+println("Hello world\n");
+
+
+
